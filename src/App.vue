4d0a0937da45d68e7e8ab2157497659578b1288b
Shakil Khan <shakilkhan621@gmail.com>
2020-10-27 07:43:22 +0500

    first part uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
+<Nav />
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
   </div>
   <router-view />
 </template>
+<script>
+import Nav from "./views/Nav";
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
 
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding:0;
+  box-sizing: border-box;
 }
-
-#nav {
-  padding: 30px;
+body {
+  font-family: sans-serif;
+  background: #eaeded;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.container {
+  width: 1300px;
+  margin: 0 auto;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+.col-4 {
+  width: 33.33333333333333%;
 }
 </style>
